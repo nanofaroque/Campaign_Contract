91696a02dc130281912b7b8bc80f128a7318365d
Omar Faroque <faroque.ae@gmail.com>
2017-11-26 13:15:40 -0600

    add control

--- Campaign.sol
+++ Campaign.sol
@@ -10,6 +10,8 @@ contract Campaign {
 
     uint    public fundsRaised;
 
+    bool    public running;
+
     // Funder object. Each of the funder will have thier address and amount
     struct FundersStruct {
     address funder;
@@ -21,16 +23,19 @@ contract Campaign {
     * Creating Events
     */
     event LogContribution(address sender, uint amount);
+
     event LogRefundSent(address funder, uint amount);
+
     event LogWithdrawal(address beneficiary, uint amount);
 
     /**
-    * constructor
+    * @constructor
     */
-    function Campaign(uint duration, uint lgoal) public{
+    function Campaign(uint duration, uint lgoal){
         owner = msg.sender;
         deadline = block.number + duration;
         goal = lgoal;
+        running = true;
     }
 
     // constant function executes locally, it wont run on block chain
@@ -49,19 +54,30 @@ contract Campaign {
         return (fundsRaised < goal && block.number > deadline);
     }
 
+    function runSwitch(bool onOff)
+    public
+    returns (bool success){
+        if(msg.sender!=owner) throw;
+        running = onOff;
+        return true;
+
+    }
     // payable--> it accepts wei
     //memory tells solidity to create a chunk of space for the variable at method runtime, guaranteeing its size and structure for future use in that method.
     function contribute()
     public
     payable
     returns (bool success){
-        require(msg.value != 0);
+        if (msg.value == 0) throw;
+        if (isSuccess()) throw;
+        if (hasFailed()) throw;
+        if (!running) throw;
         fundsRaised += msg.value;
         FundersStruct memory newFunder;
         newFunder.funder = msg.sender;
         newFunder.amount = msg.value;
         funderStructs.push(newFunder);
-        LogContribution(msg.sender,msg.value);
+        LogContribution(msg.sender, msg.value);
         return true;
 
     }
@@ -69,14 +85,17 @@ contract Campaign {
     function withdrawFunds()
     public
     returns (bool success){
-        require(msg.sender == owner) ;
-        require(isSuccess());
+        if (msg.sender != owner) {
+            throw;
+        }
+        if (!isSuccess()) throw;
+        if(!running) throw;
         // todo: check the Campaign
         // balance is a property of fund stored in the contract
         uint amount = this.balance;
         // send to the owner
-        require(owner.send(amount));
-        LogWithdrawal(owner,this.balance);
+        if(!owner.send(amount)) throw;
+        LogWithdrawal(owner, this.balance);
         return true;
 
     }
@@ -84,12 +103,12 @@ contract Campaign {
     function sendRefunds()
     public
     returns (bool success){
-        require(hasFailed());
-        require(msg.sender == owner);
+        if (!hasFailed()) throw;
+        if (msg.sender != owner) throw;
         uint funderCount = funderStructs.length;
         for (uint i = 0; i < funderCount; i++) {
-            require(funderStructs[i].funder.send(funderStructs[i].amount));
-            LogRefundSent(funderStructs[i].funder,funderStructs[i].amount);
+            funderStructs[i].funder.send(funderStructs[i].amount);
+            LogRefundSent(funderStructs[i].funder, funderStructs[i].amount);
         }
         return true;
     }
